588ccb05b75de4c214960af5372c64ba6a9eaddc
Hyosang Kang <hyosang@dgist.ac.kr>
2022-04-29 15:16:02 +0900

    update rk

--- numeric-solver/main.go
+++ numeric-solver/main.go
@@ -1,3 +1,17 @@
-package numeric_solver
+package main
 
-func main() {}
+import (
+	"fmt"
+	"main/rk"
+	"math"
+)
+
+func main() {
+	fs := []func(float64, []float64) float64{
+		func(t float64, ys []float64) float64 {
+			return math.Sin(t)
+		},
+	}
+	tl, yl := rk.RungeKutta(fs, [2]float64{0, 1}, []float64{0}, 0.01)
+	fmt.Println(tl)
+}
