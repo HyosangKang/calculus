99c25b3402ed005f1207b69ef5f970a9eb43f228
Hyosang Kang <hyosang@dgist.ac.kr>
2022-04-29 16:14:58 +0900

    add rungekutta

--- numeric-solver/main.go
+++ numeric-solver/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"main/plot"
 	"main/rk"
-	"math"
 )
 
 func main() {
-	fs := []func(float64, []float64) float64{
-		func(t float64, ys []float64) float64 {
-			return math.Sin(t)
-		},
+	f1 := func(t float64, ys []float64) float64 {
+		return -ys[1]
 	}
-	tl, yl := rk.RungeKutta(fs, [2]float64{0, 1}, []float64{0}, 0.01)
-	fmt.Println(tl)
+	f2 := func(t float64, ys []float64) float64 {
+		return -ys[0]
+	}
+	fs := []func(float64, []float64) float64{f1, f2}
+	tl, yl := rk.RungeKutta(fs, [2]float64{0, 1}, []float64{10, 5}, 0.01)
+	plot.Plot(tl, yl)
 }
--- numeric-solver/rk/rk.go
+++ numeric-solver/rk/rk.go
@@ -1,50 +1,50 @@
-package runge_kutta
+package rk
 
 func RungeKutta(fs []func(float64, []float64) float64, r [2]float64, y0 []float64, h float64) ([]float64, [][]float64) {
 	if len(y0) != len(fs) {
 		panic("The sizes of outputs do not match!")
 	}
+	n := len(y0)
 	tl := []float64{r[0]}
-	yl := [][]float64{y0}
-	ys := y0
+	yl := make([][]float64, n)
+	for i := 0; i < n; i++ {
+		yl[i] = []float64{y0[i]}
+	}
+	ys := make([]float64, n)
+	copy(ys, y0)
 	for t := r[0]; t <= r[1]-h; t += h {
 		k := make([][]float64, 4)
 		for j := 0; j < 4; j++ {
 			k[j] = make([]float64, len(y0))
 			tp := t
 			yp := make([]float64, len(ys))
+			copy(yp, ys)
 			switch j {
-			case 0:
-				for l, y := range ys {
-					yp[l] = y
-				}
 			case 1:
 				tp += h / 2
-				for l, y := range ys {
-					yp[l] = y + h*k[0][l]/2
+				for i := 0; i < n; i++ {
+					yp[i] += h * k[0][i] / 2
 				}
 			case 2:
 				tp += h / 2
-				for l, y := range ys {
-					yp[l] = y + h*k[1][l]/2
+				for i := 0; i < n; i++ {
+					yp[i] += h * k[1][i] / 2
 				}
 			case 3:
 				tp += h
-				for l, y := range ys {
-					yp[l] = y + h*k[2][l]
+				for i := 0; i < n; i++ {
+					yp[i] += h * k[2][i]
 				}
 			}
 			for i, f := range fs {
 				k[j][i] = f(tp, yp)
 			}
 		}
-		ny := []float64{}
-		for i, y := range ys {
-			ys[i] = y + h*(k[0][i]+2*k[1][i]+2*k[2][i]+k[3][i])/6
-			ny = append(ny, ys[i])
+		for i := 0; i < n; i++ {
+			ys[i] += h * (k[0][i] + 2*k[1][i] + 2*k[2][i] + k[3][i]) / 6
+			yl[i] = append(yl[i], ys[i])
 		}
 		tl = append(tl, t+h)
-		yl = append(yl, ny)
 	}
 	return tl, yl
 }
